1efdbfe4bfc517fb5d9acef06cc7974e83889022
(RAS Hungary) Szaniszló Ivor <szaniszlo.ivor@ringier.hu>
2022-10-23 22:58:12 +0200

    FIX: express path, ADD: implement OAuth Routes

--- app.js
+++ app.js
@@ -48,11 +48,11 @@ passport.deserializeUser((user, done) => {
 /*
  *  Express Project Setup
 */
-app.set('views', __dirname + '/views');
+app.set("views", path.join(__dirname, "/views"));
 app.set('view engine', 'ejs');
 app.use(partials());
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, "/public")));
 app.use(session({
   secret: 'codecademy',
   resave: false,
@@ -68,12 +68,11 @@ app.use(passport.session());
 /*
  * Routes
 */
-
 app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 })
 
-app.get('/account', (req, res) => {
+app.get('/account', ensureAuthenticated, (req, res) => {
   res.render('account', { user: req.user });
 });
 
@@ -86,16 +85,28 @@ app.get('/logout', (req, res) => {
   res.redirect('/');
 });
 
+app.get('/auth/github', passport.authenticate('github', { scope: ['user'] }));
 
 
+/*
+ * ensureAuthenticated Callback Function (error, success)
+*/
+app.get('/auth/github/callback', 
+  passport.authenticate('github', {
+    failureRedirect: '/login',
+    successRedirect: '/',
+  })
+);
 
 /*
  * Listener
 */
-
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-/*
- * ensureAuthenticated Callback Function
-*/
 
+const ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next()
+  } 
+  res.redirect('/redirect');
+} 
